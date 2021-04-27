832ccdd5156325685e8e21a14a7457bca3605316
Ahmad <gracenoteii5@gmail.com>
2021-04-27 22:42:51 +0500

    Login Gui Created

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -1,23 +1,24 @@
 package Classes;
-
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/Views/Login.fxml"));
         primaryStage.setTitle("Log In");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,4 +1,28 @@
 package Controller;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController
+{
+    @FXML
+    private TextField Userfield;
+    @FXML
+    private PasswordField Passwordfield;
+    @FXML
+    private Label Errorlabel;
+
+    public void onLoginClick()
+    {
+        System.out.println(Userfield.getText());
+        System.out.println(Passwordfield.getText());
+        Errorlabel.setVisible(true);
+    }
+
+    public void onRegisterClick()
+    {
+        System.out.println("Welcome To Registration");
+    }
 }
